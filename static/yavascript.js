b73f3e872f44f820ab1bb03031d592192905ba95
Elliot Snow-Kropla <esnowkropla@gmail.com>
2017-07-13 15:26:38 -0400

    Homepage now updates with the list of posts

--- static/yavascript.js
+++ static/yavascript.js
@@ -1,11 +1,12 @@
 var author = document.getElementById("name");
 var summary = document.getElementById("summary");
 var contents = document.getElementById("contents");
+var display = document.getElementById("display");
 
 var button = document.getElementById("submit");
 button.onclick = function() {
 	var request = new XMLHttpRequest();
-	request.open('POST', "http://localhost:3000/post");
+	request.open('POST', "http://192.168.2.18:3000/post");
 	request.setRequestHeader("Content-type", "application/json");
 	request.onload = function() {
 		alert(request.responseText);
@@ -17,12 +18,45 @@ button.onclick = function() {
 
 function ping() {
 	var request = new XMLHttpRequest();
-	request.open('GET', "http://localhost:3000/feed");
+	request.open('GET', "http://192.168.2.18:3000/feed");
 	request.setRequestHeader("Content-type", "application/json");
 	request.onload = function() {
-		alert(request.responseText);
+		fill_feed(request.responseText);
 	};
 	request.send();
 	setTimeout(ping, 5000);
 }
-//ping();
+ping();
+
+function fill_feed(responseText) {
+	var list = JSON.parse(responseText);
+	list.reverse();
+	
+	clear_display();
+	list.forEach(function(item, index, array) {
+		add_to_display(item);
+	});
+}
+
+function add_to_display(item) {
+	var para = document.createElement("p");
+	var name = document.createTextNode(item.author_handle + ": ");
+	var link = document.createElement("a");
+	var linkText = document.createTextNode(item.summary);
+	link.appendChild(linkText);
+	link.href = "http://192.168.2.18:3000/post/" + item.uuid;
+
+	para.appendChild(name);
+	para.appendChild(link);
+
+	display.appendChild(para);
+}
+
+function clear_display() {
+	var elements = display.getElementsByTagName("p");
+	while (elements.length > 0) {
+		var node = elements[0];
+		display.removeChild(node);
+		node.nodeValue = "";
+	}
+}
